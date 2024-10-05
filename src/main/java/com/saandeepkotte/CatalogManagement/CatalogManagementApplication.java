4257041e383f3a90bf86e9780c7c6cd68f63e5c6
Saandeep Kotte <saandeepkotte5@gmail.com>
2024-10-05 14:53:35 +0530

    project init

---
+++ src/main/java/com/saandeepkotte/CatalogManagement/CatalogManagementApplication.java
@@ -0,0 +1,13 @@
+package com.saandeepkotte.CatalogManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saandeepkotte/CatalogManagement/CatalogManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saandeepkotte.CatalogManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
