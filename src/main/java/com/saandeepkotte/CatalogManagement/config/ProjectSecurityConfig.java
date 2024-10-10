ffc6a34c392179e4641f70d8098bc4be7c887b08
Saandeep Kotte <saandeepkotte5@gmail.com>
2024-10-10 23:30:56 +0530

    cors config

--- src/main/java/com/saandeepkotte/CatalogManagement/config/ProjectSecurityConfig.java
+++ src/main/java/com/saandeepkotte/CatalogManagement/config/ProjectSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +35,7 @@ public class ProjectSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf((customizer) -> customizer.disable())
+                .cors((customizer) -> customizer.disable())
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/user/register", "/api/user/login")
                         .permitAll()
